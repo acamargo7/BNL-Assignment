483aad88cb71c23e1df9e4a6e8760f7aa704aeef
elbebegtrs <elbebegtrs@hotmail.com>
2024-04-24 17:09:07 -0400

    additional functionality. Added search

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,22 +9,12 @@ import * as FileSaver from 'file-saver';
 import { TableModule } from 'primeng/table';
 import { ButtonModule } from 'primeng/button';
 import { TooltipModule } from 'primeng/tooltip';
-
-interface Column {
-  field: string;
-  header: string;
-  customExportHeader?: string;
-}
-
-interface ExportColumn {
-  title: string;
-  dataKey: string;
-}
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, DecimalPipe, DatePipe, CurrencyPipe, NgbDatepickerModule, FormsModule, JsonPipe, NgStyle, CurrencyPipe, TableModule, ButtonModule, TooltipModule],
+  imports: [RouterOutlet, DecimalPipe, DatePipe, CurrencyPipe, NgbDatepickerModule, FormsModule, JsonPipe, NgStyle, CurrencyPipe, TableModule, ButtonModule, TooltipModule, InputTextModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -33,33 +23,34 @@ export class AppComponent implements OnInit {
   model!: NgbDateStruct;
 
   public fruits: Fruit[] = [];
-  public filteredFruits: Fruit[] = [];
 
-  public filteredFruits2: Fruit[] = [];
+  public filteredFruits: Fruit[] = [];
+  public searchValue: string;
 
   constructor(private fruitService: FruitService) {
     this.fruits = this.fruitService.getFruits();
+    this.searchValue = '';
   }
 
   ngOnInit() {
     this.filteredFruits = [...this.fruits];
-    this.filteredFruits2 = [...this.fruits];
   }
 
   public updateBasedOnDate($event: NgbDateStruct) {
     let date = new Date($event.year, $event.month-1, $event.day);
     let newList = this.fruits.filter(x => formatDate(x.datePicked, 'yyyy-MM-dd', 'en_US') == formatDate(date, 'yyyy-MM-dd', 'en_US'));
-    this.filteredFruits2 = [...newList];
+    this.filteredFruits = [...newList];
   }
 
   public reset() {
-    this.filteredFruits2 = [...this.fruits];
+    this.filteredFruits = [...this.fruits];
+    this.searchValue = '';
   }
 
   public exportExcel() {
     //need to install npm i xlsx
     import('xlsx').then((xlsx) => {
-      const worksheet = xlsx.utils.json_to_sheet(this.filteredFruits2);
+      const worksheet = xlsx.utils.json_to_sheet(this.filteredFruits);
       const workbook = { Sheets: { data: worksheet }, SheetNames: ['data'] };
       const excelBuffer: any = xlsx.write(workbook, { bookType: 'xlsx', type: 'array' });
       this.saveAsExcelFile(excelBuffer, 'products');
@@ -73,5 +64,26 @@ export class AppComponent implements OnInit {
       type: EXCEL_TYPE
     });
     FileSaver.saveAs(data, fileName + '_export_' + new Date().getTime() + EXCEL_EXTENSION);
+  }
+
+  public search(item: any) {
+    console.log(item);
+    console.log(this.fruits);
+    let searchTerm = item.toLowerCase();
+
+    if (searchTerm === '') {
+      this.filteredFruits = [...this.fruits];
+    } else {
+      let filterList = this.fruits.filter(fruit => {
+        let datePicked = formatDate(fruit.datePicked, 'M/d/yy', 'en_US');
+        if (fruit.color.toLowerCase().includes(searchTerm)) { return true; }
+        else if (fruit.price.toString().includes(searchTerm)) { return true }
+        else if (datePicked.includes(searchTerm)) { return true; }
+        else if (fruit.weight == searchTerm) { return true; }
+        else { return false; }
+      });
+
+      this.filteredFruits = [...filterList];
+    }
   }
 }
